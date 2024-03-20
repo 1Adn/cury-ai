1c18f200c26a550893040185a9959edf0f45d18e
Yannis Djaffer <yannis.djaffer@gmail.com>
2024-03-20 18:10:45 +0100

    Adding last details

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="form"
 export default class extends Controller {
-  static targets = ["step1", "step2", "step3", "step4", "step5", "step2FormLanguage", "step2FormSkill", "step3Form", "step4Form"]
+  static targets = ["step1", "step2", "step3", "step4", "step5", "step2Form", "step3FormSkill", "step3FormLanguage", "step4Form"]
 
   connect() {
     console.log("Connected")
@@ -19,14 +19,14 @@ export default class extends Controller {
     this.step3Target.classList.add("d-none");
     this.step4Target.classList.remove("d-none");
   }
-  showStep2FormLanguage() {
-    this.step2FormLanguageTarget.classList.remove("d-none")
+  showStep2Form() {
+    this.step2FormTarget.classList.remove("d-none")
   }
-  showStep2FormSkill() {
+  showStep3FormSkill() {
     this.step2FormSkillTarget.classList.remove("d-none")
   }
-  showStep3Form() {
-    this.step3FormTarget.classList.remove("d-none")
+  showStep3FormLanguage() {
+    this.step3FormLanguageTarget.classList.remove("d-none")
   }
   showStep4Form() {
     this.step4FormTarget.classList.remove("d-none")
