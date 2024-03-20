4e4ac7a800551ac8c2df15d882d7c8c279a3f302
adrien leveille <adrienleveille.contact@gmail.com>
2024-03-20 16:02:46 +0100

    Job offer => new, create, edit, update, delete

---
+++ app/controllers/job_offers_controller.rb
@@ -0,0 +1,45 @@
+class JobOffersController < ApplicationController
+
+  def new
+    @job_offer = JobOffer.new
+  end
+
+  def create
+    @job_offer = JobOffer.new(job_offer_params)
+    @job_offer.user = current_user
+    if @job_offer.save
+      redirect_to @job_offer, notice: 'Job offer was successfully added!'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @job_offer = JobOffer.find(params[:id])
+  end
+
+  def edit
+    @job_offer = JobOffer.find(params[:id])
+  end
+
+  def update
+    @job_offer = JobOffer.find(params[:id])
+    if @job_offer.update(job_offer_params)
+      redirect_to @job_offer, notice: 'Job offer was successfully updated!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job_offer = JobOffer.find(params[:id])
+    @job_offer.destroy
+    redirect_to  new_job_offer_path, notice: 'Job offer has been deleted successfully!'
+  end
+
+  private
+
+    def job_offer_params
+      params.require(:job_offer).permit(:title, :description)
+    end
+end
--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -1,3 +1,7 @@
 class JobOffer < ApplicationRecord
   belongs_to :user
+  has_many :cv_generations
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
