2eae97870c2a948e3f033e6d144fb951fd1c376e
Yannis Djaffer <yannis.djaffer@gmail.com>
2024-03-28 17:29:15 +0100

    jsp

--- app/controllers/cvs_controller.rb
+++ app/controllers/cvs_controller.rb
@@ -1,3 +1,14 @@
 class CvsController < ApplicationController
+  # def download
+  # @cv_data = Cv.find(params[:id]) # Assure-toi d'adapter cette ligne en fonction de ta logique
 
+  # respond_to do |format|
+  #   format.html
+  #   format.pdf do
+  #     render pdf: "cv",
+  #            template: "cv/download.pdf.erb", # Assure-toi d'avoir un fichier de vue correspondant
+  #            layout: 'pdf.html.erb' # Facultatif : Utilise un layout PDF personnalisé si nécessaire
+  #   end
+  # end
+  # end
 end
